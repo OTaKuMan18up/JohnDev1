2ff0707490cab139f8a2abdfde62f2c75eb3c062
Watthanakrij <john.master.cyber@gmail.com>
2025-04-29 21:46:51 +0700

    test

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-alert('Hack!');
-console.log(document.cookie);
-
-var phpData = "<?php echo 'Injected by PHP'; ?>";
-console.log(phpData);
+alert('Update ci_session cookie value!');
+// ฟังก์ชันช่วยอ่าน Cookie
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+  
+  // ดึงค่า ci_session
+  const ciSession = getCookie('ci_session');
+  console.log(ciSession);
+  
